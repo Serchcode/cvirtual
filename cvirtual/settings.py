7529031138752d07af1b59fbe639df4090eae7ff
Serchcode <pe280591@uaeh.edu.mx>
2018-01-29 21:14:23 -0600

    heroku2

--- cvirtual/settings.py
+++ cvirtual/settings.py
@@ -123,11 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+#STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,"static"),)
-#STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles/")
-
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles/")
 MEDIA_ROOT = os.path.join(BASE_DIR,'media').replace('\\', '/')
 MEDIA_URL = '/media/'
 CKEDITOR_UPLOAD_PATH = 'uploads/'
