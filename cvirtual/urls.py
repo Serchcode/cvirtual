0cce430e52e21f2f0fa1d25f54ed8486dab97e37
Serchcode <pe280591@uaeh.edu.mx>
2018-01-29 20:52:40 -0600

    bug debug=false

--- cvirtual/urls.py
+++ cvirtual/urls.py
@@ -4,9 +4,9 @@ from main import urls as homeUrls
 from blog import urls as blogUrls
 from django.views.static import serve
 from django.conf import settings
+from django.contrib.staticfiles import views
 from django.conf.urls import handler404, handler500
 from . import views
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -15,6 +15,7 @@ urlpatterns = [
     #url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(regex=r'^media/(?P<path>.*)$',view=serve,kwargs={'document_root':settings.MEDIA_ROOT}),
 ]
+
 
 handler404 = views.error_404
 handler500 = views.error_500
